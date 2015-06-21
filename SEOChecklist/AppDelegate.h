cb377d7597a98c1252cd76b829109878b7348d3f
Tyler Hunnefeld <tylerhunnefeld@Tylers-Mac-Pro.local>
2015-06-21 09:38:20 -0400

    Initial Commit

---
+++ SEOChecklist/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SEOChecklist
+//
+//  Created by Tyler Hunnefeld on 6/21/15.
+//  Copyright (c) 2015 Tyler Hunnefeld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SEOChecklist/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SEOChecklist
+//
+//  Created by Tyler Hunnefeld on 6/21/15.
+//  Copyright (c) 2015 Tyler Hunnefeld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
